88137eb8de6bfe0a8b5ec53458059566626bc3e6
Cabaljos000 <127070153+Cabaljos000@users.noreply.github.com>
2024-09-10 15:04:48 +0000

    update 1

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, Jose!");
+fn pattern_match_simple() {
+    let num: i32 = 3;
+    let letter: char = match num {
+        1 => 'A',
+        2 => 'B',
+        3 => {'C'},
+        _ => '#', // rust will not guess
+    };
+    println!("{}", letter);
 }
+
+fn main() {
+    pattern_match_simple();
+}
+// Try to go through mod 2. 1-3
