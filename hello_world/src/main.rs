9967889186474cc9ddcafea212349d8d6556c30f
Cabaljos000 <127070153+Cabaljos000@users.noreply.github.com>
2024-09-19 02:12:40 +0000

    Guessing game

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,40 +1,36 @@
-fn is_even(n: i32) -> bool {
-    n % 2 == 0
+fn check_guess(guess: i32, secret:i32) -> i32 {
+    if guess == secret {
+        0
+    } else if guess > secret {
+        1
+    } else {
+        -1
+    }
 }
 
-fn main() {
-    let nums = [10, 15, 23, 30, 42, 5, 60, 75, 12, 9];
+fn main(){
+    let secret_number = 61;
 
-    for &num in nums.iter(){
-        if is_even(num) {
-            println!("{} is even", num);
+    let mut attempts = 0;
+
+    let mut guess = 56;
+
+    loop{
+        attempts += 1;
+
+        let result = check_guess(guess, secret_number);
+
+        if result == 0 {
+            println!("You guessed it! The secret number is {}", secret_number);
+            break;
+        } else if result == 1 {
+            println!("Your guess of {} is too high!", guess);
+            guess -= 1;
         } else {
-            println!("{} is odd", num);
-        }
-
-        if num % 3 == 0 && num % 5 == 0 {
-            println!("FizzBuzz");
-        } else if num % 3 == 0 {
-            println!("Fizz");
-        } else if num % 5 == 0 {
-            println!("Buzz");
-        }
+            println!("Your guess of {} is too low!", guess);
+            guess += 1;
+        }        
     }
 
-    let mut sum = 0;
-    let mut _i = 0;
-    
-    while _i < nums.len() {
-        sum += nums[_i];
-        _i += 1;
-    }
-    println!("The sum of all numbers is: {}", sum);
-
-    let mut largest = nums[0];
-    for &num in nums.iter() {
-        if num > largest {
-            largest = num;
-        }
-    }
-    println!("The largest number is: {}", largest);
+    println!("You found the number in {} attempts.", attempts);
 }
