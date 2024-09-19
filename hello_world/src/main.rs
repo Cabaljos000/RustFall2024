d935768423e81ac2eb3b62482edba46d74295798
Cabaljos000 <127070153+Cabaljos000@users.noreply.github.com>
2024-09-19 01:32:07 +0000

    Temperature Converter

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,15 +1,30 @@
-fn pattern_match_simple() {
-    let num: i32 = 3;
-    let letter: char = match num {
-        1 => 'A',
-        2 => 'B',
-        3 => {'C'},
-        _ => '#', // rust will not guess
-    };
-    println!("{}", letter);
+const FREEZING_POINT: f64 = 32.0;
+
+fn fahrenheit_to_celsius(f: f64) -> f64{
+    (f - FREEZING_POINT) * 5.0 / 9.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0 / 5.0) + FREEZING_POINT
 }
 
 fn main() {
-    pattern_match_simple();
+    let mut temp_fahrenheit: f64 = FREEZING_POINT;
+
+    let mut temp_celsius = fahrenheit_to_celsius(temp_fahrenheit);
+    println!("{}°F is equal to {:.2}°C",temp_fahrenheit, temp_celsius);
+
+    for _i in 1..=5 {
+        temp_fahrenheit += 1.0;
+        temp_celsius = fahrenheit_to_celsius(temp_fahrenheit);
+        println!("{}°F is equal to {:.2}°C",temp_fahrenheit, temp_celsius);
+    }
+
+    temp_celsius = 3.0;
+
+    for _i in 1..=5 {
+        temp_celsius += 1.0;
+        temp_fahrenheit = celsius_to_fahrenheit(temp_celsius);
+        println!("{}°C is equal to {:.2}°F", temp_celsius, temp_fahrenheit);
+    }
 }
-// Try to go through mod 2. 1-3
