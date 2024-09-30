065d5e5c3281792b219b8a9e9fdccef092b40afc
Cabaljos000 <127070153+Cabaljos000@users.noreply.github.com>
2024-09-30 22:46:54 +0000

    I/O operation

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,36 +1,72 @@
-fn check_guess(guess: i32, secret:i32) -> i32 {
-    if guess == secret {
-        0
-    } else if guess > secret {
-        1
-    } else {
-        -1
+use std::fs::File;
+use std::io::{self, Write, BufReader, BufRead};
+use std::path::Path;
+
+struct Car {
+    make: String,
+    model: String,
+    year: u32,
+    color: String,
+}
+
+impl Car {
+    fn new() -> Car {
+        let mut input = String::new();
+
+        print!("Enter your car make? ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        let make = input.trim().to_string();
+        input.clear();
+    
+        print!("Enter your car model? ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        let model = input.trim().parse().unwrap();
+        input.clear();
+
+        print!("Enter your car year? ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        let year = input.trim().parse().unwrap();
+        input.clear();
+
+        print!("Enter your car color? ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        let color = input.trim().parse().unwrap();
+        
+        Car {make, model, year, color}
+    }
+
+    fn save_to_file(&self, filename: &str){
+        let mut file = File::create(filename).expect("Unable to create file");
+        writeln!(file, "Make: {}", self.make).expect("Unable to write to file");
+        writeln!(file, "Model: {}", self.model).expect("Unable to write to file");
+        writeln!(file, "Year: {}", self.year).expect("Unable to write to file");
+        writeln!(file, "Color: {}", self.color).expect("Unable to write to file");
+    }
+    
+    fn read_from_file(filename: &str){
+        if Path::new(filename).exists(){
+            let file = File::open(filename).expect("Unable to open file");
+            let reader = BufReader::new(file);
+    
+            for line in reader.lines(){
+                println!("{}", line.unwrap());
+            }
+        } else {
+            println!("File does not exist.")
+        }
     }
 }
 
-fn main(){
-    let secret_number = 61;
+fn main() {
+    let car = Car::new();
+    let filename = "user_info.txt";
 
-    let mut attempts = 0;
+    car.save_to_file(filename);
 
-    let mut guess = 56;
-
-    loop{
-        attempts += 1;
-
-        let result = check_guess(guess, secret_number);
-
-        if result == 0 {
-            println!("You guessed it! The secret number is {}", secret_number);
-            break;
-        } else if result == 1 {
-            println!("Your guess of {} is too high!", guess);
-            guess -= 1;
-        } else {
-            println!("Your guess of {} is too low!", guess);
-            guess += 1;
-        }        
-    }
-
-    println!("You found the number in {} attempts.", attempts);
+    println!("\nContent of the file '{}':", filename);
+    Car::read_from_file(filename);
 }
