bce4f709d316df59b1479bccbb3980a0fad7c699
Cabaljos000 <127070153+Cabaljos000@users.noreply.github.com>
2024-12-04 00:05:32 +0000

    Spawn 3 Threads

---
+++ threads/target/debug/deps/threads-9d45826e428809f3.d
@@ -0,0 +1,5 @@
+/workspaces/RustFall2024/threads/target/debug/deps/threads-9d45826e428809f3: src/main.rs
+
+/workspaces/RustFall2024/threads/target/debug/deps/threads-9d45826e428809f3.d: src/main.rs
+
+src/main.rs:
---
+++ threads/target/debug/threads.d
@@ -0,0 +1,1 @@
+/workspaces/RustFall2024/threads/target/debug/threads: /workspaces/RustFall2024/threads/src/main.rs
